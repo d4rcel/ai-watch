a4d3c292bd5c0543ec0e7271dec79ba1cf650306
d4rcel <103891322+d4rcel@users.noreply.github.com>
2025-07-16 11:11:06 +0100

    chore: implement transform data for formating

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,7 +1,13 @@
 from collectors.rss_collector import collect_rss_feeds
+from transformers.data_transformer import transform_data
 
 def app():
-    pass
+    print("Starting the AI watch agent...")
+    while True:
+        articles = collect_rss_feeds()
+        if articles:
+            transformed_articles = transform_data(articles)
+            break
 
 
 if __name__ == "__main__":
